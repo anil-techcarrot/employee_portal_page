8afb7ff4fd419ce628c9cc188f6362fceabb2910
George Vincent A <george.ifensys@gmail.com>
2025-05-27 12:20:23 +0530

    bug fixes and enhancement done

--- techcarrot_employee/models/tec_expense.py
+++ techcarrot_employee/models/tec_expense.py
@@ -11,3 +11,12 @@ class HrExpense(models.Model):
 
     emp_code = fields.Char('Employee Code', copy=False)
     project_id = fields.Many2one('project.project', 'Project', copy=False)
+
+    @api.model
+    def create(self, vals):
+        if 'emp_code' in vals and not vals.get('employee_id'):
+            emp_code = vals['emp_code']
+            employee = self.env['hr.employee'].search([('emp_code', '=', emp_code)], limit=1)
+            if employee:
+                vals['employee_id'] = employee.id
+        return super(HrExpense, self).create(vals)
