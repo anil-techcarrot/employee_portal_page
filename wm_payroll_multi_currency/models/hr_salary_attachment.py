c52fc5d3a57edf566aa75cee32da82e4c8d1e8ba
techcarrot <147490236+techcarrotOdoo@users.noreply.github.com>
2026-05-22 14:32:48 +0400

    Comment out _compute_estimated_end method
    commented line 22 to 31

--- wm_payroll_multi_currency/models/hr_salary_attachment.py
+++ wm_payroll_multi_currency/models/hr_salary_attachment.py
@@ -19,13 +19,13 @@ class HrSalaryAttachment(models.Model):
     #     if any(attachment.currency_id != attachment.employee_ids[0].contract_id.currency_id for attachment in self.filtered(lambda x: x.employee_count == 1)):
     #         raise ValidationError(_("Salary attachment currency not match employee current contract currency."))
 
-    @api.depends('state', 'total_amount', 'monthly_amount', 'date_start')
-    def _compute_estimated_end(self):
-        for record in self:
-            if record.state not in ['close', 'cancel'] and record.has_total_amount and record.monthly_amount:
-                record.date_estimated_end = start_of(record.date_start + relativedelta(months=ceil(record.remaining_amount / record.monthly_amount)), 'month')
-                record.date_end = start_of(record.date_start + relativedelta(months=ceil(record.remaining_amount / record.monthly_amount)),
-                    'month')
-            else:
-                record.date_estimated_end = False
-                record.date_end = False
+    # @api.depends('state', 'total_amount', 'monthly_amount', 'date_start')
+    # def _compute_estimated_end(self):
+    #     for record in self:
+    #         if record.state not in ['close', 'cancel'] and record.has_total_amount and record.monthly_amount:
+    #             record.date_estimated_end = start_of(record.date_start + relativedelta(months=ceil(record.remaining_amount / record.monthly_amount)), 'month')
+    #             record.date_end = start_of(record.date_start + relativedelta(months=ceil(record.remaining_amount / record.monthly_amount)),
+    #                 'month')
+    #         else:
+    #             record.date_estimated_end = False
+    #             record.date_end = False
